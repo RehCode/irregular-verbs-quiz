dc09364988922547ef05eca745e3877ef3d468ef
René Hernández <rene.rhc@gmail.com>
2019-01-31 17:17:08 -0700

    Updates

---
+++ precache-manifest.2562697ba0d562ee17a4b6842ba1fefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38753f3c72bc91f88d62",
+    "url": "/irregular-verbs-quiz/static/js/runtime~main.38753f3c.js"
+  },
+  {
+    "revision": "0bfb3a4e9c397e95a347",
+    "url": "/irregular-verbs-quiz/static/js/main.0bfb3a4e.chunk.js"
+  },
+  {
+    "revision": "d78a20f7712093c3f480",
+    "url": "/irregular-verbs-quiz/static/js/1.d78a20f7.chunk.js"
+  },
+  {
+    "revision": "0bfb3a4e9c397e95a347",
+    "url": "/irregular-verbs-quiz/static/css/main.9228f83b.chunk.css"
+  },
+  {
+    "revision": "172e7caac57651eea4b5854f33b314bc",
+    "url": "/irregular-verbs-quiz/index.html"
+  }
+];
